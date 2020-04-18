21e509555ab31de247c2417570abd2bcf545c557
Miguel Sánchez <mschezes@gmail.com>
2020-04-18 15:36:16 +0200

    Fix production script

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,5 +1,4 @@
 /* eslint-disable import/no-extraneous-dependencies */
-const webpack = require('webpack');
 const merge = require('webpack-merge');
 
 const baseConfig = require('./webpack.config.base');
@@ -8,6 +7,10 @@ module.exports = merge(
   baseConfig,
   {
     entry: ['./src/SideBar.js'],
+    mode: 'production',
+    optimization: {
+      minimize: true,
+    },
     output: {
       filename: './dist/SideBar.js',
       library: 'SideBar',
@@ -15,21 +18,5 @@ module.exports = merge(
       path: __dirname,
       umdNamedDefine: true,
     },
-    plugins: [
-      new webpack.optimize.UglifyJsPlugin({
-        compress: {
-          pure_getters: true,
-          screw_ie8: true,
-          unsafe: true,
-          unsafe_comps: true,
-          warnings: false,
-        },
-        exclude: [/\.min\.js$/gi],
-        mangle: true,
-        output: {
-          comments: false,
-        },
-      }),
-    ],
   },
 );
